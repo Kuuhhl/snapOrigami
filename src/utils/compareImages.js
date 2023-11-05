951e77cf1ef7fb15695ce1ce98ac693308678dfc
philipp <50250716+Kuuhhl@users.noreply.github.com>
2023-11-05 05:47:19 +0000

    modified image recognition algorithm

--- src/utils/compareImages.js
+++ src/utils/compareImages.js
@@ -4,6 +4,8 @@ export default async function compareImages(image1, image2) {
 	const diff = await new Promise((resolve) => {
 		Resemble(image1)
 			.compareTo(image2)
+			.scaleToSameSize()
+			.ignoreAntialiasing()
 			.ignoreColors()
 			.onComplete((data) => {
 				resolve(data);
